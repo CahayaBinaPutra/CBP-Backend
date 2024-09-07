a06d2fb25de5f0e56f7071c728d60e61dcc55c8e
CahayaBinaPutra <bprcbt7@gmail.com>
2024-09-07 08:36:42 -0700

    up

--- server.js
+++ server.js
@@ -6,26 +6,31 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions = {
-  origin: 'https://CahayaBinaPutra.github.io', // Ganti dengan URL frontend kamu
+  origin: 'https://cahayabinaputra.github.io', // Ganti dengan URL frontend kamu
   methods: 'GET,POST',
   allowedHeaders: 'Content-Type',
 };
 
 app.use(cors(corsOptions));
+
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
 
-// Define your routes
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
 const feedbackRoutes = require('./routes/feedback');
 const searchRoutes = require('./routes/search');
 const chatbotRoutes = require('./routes/chatbot');
 
+
 app.use('/api/feedback', feedbackRoutes);
 app.use('/api/chatbot', chatbotRoutes);
 app.use('/api', searchRoutes);
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
