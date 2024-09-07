8dd73bc474f8904a0c15aa265beb9fc955d77634
CahayaBinaPutra <bprcbt7@gmail.com>
2024-09-07 08:10:08 -0700

    up

--- server.js
+++ server.js
@@ -12,23 +12,20 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
+// Define your routes
 const feedbackRoutes = require('./routes/feedback');
 const searchRoutes = require('./routes/search');
 const chatbotRoutes = require('./routes/chatbot');
 
-
 app.use('/api/feedback', feedbackRoutes);
 app.use('/api/chatbot', chatbotRoutes);
 app.use('/api', searchRoutes);
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
